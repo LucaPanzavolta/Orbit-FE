6f3eebe8e29bb39aa822224ef5402959870929cc
cervante <luca.panzavolta@gmail.com>
2019-01-22 11:32:59 +0100

    Main structure of the frontend completed.

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ApiService {
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  baseUrl = "http://localhost:3000";
+  token = "0a2b9680-1bda-11e9-8fc5-4df11c3c732b";
+
+  login(email, password): Observable<any> {
+    let base64Encoded = btoa(`${email}:${password}`);
+
+    let headers = new HttpHeaders({
+      "Content-Type": "application/json",
+      "Authorization": `Basic ${base64Encoded}`
+    });
+    console.log('headers from login', headers);
+
+    return this.http.get(`${this.baseUrl}/log-in`, { headers: headers })
+      .pipe(catchError(res => res.error.errors));
+  }
+
+  //WORKSPACES
+  getWorkspaces() {
+    let headers = this.setHeadersWithToken();
+    return this.http.get(`${this.baseUrl}/dashboard`, { headers: headers });
+  }
+
+  addWorkspace(category, wsName) {
+    const payload = JSON.stringify({
+      name: wsName,
+      category: category
+    });
+
+    let headers = this.setHeadersWithToken();
+    return this.http.post(`${this.baseUrl}/dashboard`, payload, { headers: headers })
+  }
+
+  //ENTRIES
+  getEntries(workspaceId): any {
+    let headers = this.setHeadersWithToken();
+    return this.http.get(`${this.baseUrl}/dashboard/${workspaceId}`, { headers: headers });
+  }
+
+  //HELPER FUNCTION
+  setHeadersWithToken() {
+    //get token from local storage
+    let stored = JSON.parse(localStorage.getItem('currentUser'));
+    let token = stored.token;
+
+    let headers = new HttpHeaders({
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`
+    });
+
+    return headers;
+  }
+
+}
---
+++ src/app/services/store.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { Observable, Subject } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class StoreService {
+
+  public workspaces;
+  public workspaces$: Subject<any> = new Subject();
+
+  public entries;
+  private entries$: Subject<any[]> = new Subject();
+
+  private currentUser: any;
+  public currentUser$: Subject<any> = new Subject();
+
+  constructor(
+    private apiService: ApiService
+  ) { }
+
+  login(email, password) {
+    return this.apiService.login(email, password)
+      .pipe(map(res => {
+        if (res.token) {
+          localStorage.setItem('currentUser', JSON.stringify(res));
+          //setting internal private property
+          this.currentUser = res;
+          //publishing data for subscribers
+          return this.currentUser;
+        }
+        return res;
+      }));
+  }
+
+  getWorkspaces() {
+    this.apiService.getWorkspaces()
+      .subscribe(data => {
+        console.log('Retrieved workspaces in getWorkspaces (STORE S.) ', data);
+        this.workspaces = data.workspaces;
+        this.workspaces$.next(data.workspaces);
+      });
+
+    //return this.workspaces$;
+  }
+
+  addWorkspace(category, wsName) {
+    this.apiService.addWorkspace(category, wsName)
+      .subscribe(data => {
+        console.log('Retrieved data in addWorkspace (STORE S.)', data);
+        console.log('old state of workspaces ', this.workspaces);
+        this.workspaces$.next([...this.workspaces, data]);
+      });
+  }
+
+  getEntries(workspaceId) {
+    this.apiService.getEntries(workspaceId)
+      /* .pipe(map(response => { console.log('data in store service ', response); return this.entries = response }))
+      .pipe(tap((entries: any[]) => this.entries$.next(entries))); */
+      .subscribe(data => { console.log('in store service ', data); this.entries$.next(data) })
+
+    return this.entries$;
+  }
+}
